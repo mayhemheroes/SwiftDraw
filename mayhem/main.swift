68cb91dabfba6b3288576163438996fb36dc8fc4
Bailey Capuano <bcapuano@asu.edu>
2023-05-14 18:20:56 -0400

    Added more coverage

--- mayhem/main.swift
+++ mayhem/main.swift
@@ -9,13 +9,31 @@ import MSVCRT
 import Foundation
 import SwiftDraw
 
-let supported_options = [
-    SVG.Options.
-]
 @_cdecl("LLVMFuzzerTestOneInput")
 public func test(_ start: UnsafeRawPointer, _ count: Int) -> CInt {
     let fdp = FuzzedDataProvider(start, count)
     let svg_data = Data(fdp.ConsumeRemainingString().utf8)
-    SVG(data: svg_data, options: .Element)
-    return 0;
+    
+    let data_choice = fdp.ConsumeIntegralInRange(from: 0, to: 2)
+    
+    do {
+        let svg = SVG(data: svg_data)!
+        
+        switch (data_choice) {
+        case 0:
+            try svg.pngData()
+        case 1:
+            try svg.jpegData()
+        case 2:
+            try svg.pdfData()
+        default:
+            fatalError("Unsupported data choice")
+        }
+    
+        return 0;
+    }
+    catch let err {
+        print(err)
+        print(type(of: err))
+    }
 }
