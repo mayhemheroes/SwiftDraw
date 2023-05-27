70dd4b0f76d8cc4c8e72a1443cda1a6a990a5cea
bcapuano <bcapuano@asu.edu>
2023-05-26 20:38:43 -0400

    Changes

--- mayhem/main.swift
+++ mayhem/main.swift
@@ -17,24 +17,24 @@ public func test(_ start: UnsafeRawPointer, _ count: Int) -> CInt {
     let data_choice = fdp.ConsumeIntegralInRange(from: 0, to: 2)
     
     do {
-        let svg = SVG(data: svg_data)!
-        
-        switch (data_choice) {
-        case 0:
-            try svg.pngData()
-        case 1:
-            try svg.jpegData()
-        case 2:
-            try svg.pdfData()
-        default:
-            fatalError("Unsupported data choice")
+        if let svg = SVG(data: svg_data) {
+            switch (data_choice) {
+            case 0:
+                try svg.pngData()
+            case 1:
+                try svg.jpegData()
+            case 2:
+                try svg.pdfData()
+            default:
+                fatalError("Unsupported data choice")
+            }
         }
-    
         return 0;
     }
     catch let err {
         print(err)
         print(type(of: err))
+        exit(EXIT_FAILURE)
     }
     return 0;
 }
